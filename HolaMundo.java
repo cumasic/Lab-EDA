d55d4a24b616325be4a952ebd6b154c9ecfa4924
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-05-06 17:20:04 -0500

    Bienvenida al curso

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,5 +3,6 @@ public class HolaMundo
     public static void main(String args[]) {
         /** */
         System.out.println ("¡Hola mundo!");
+        System.out.println ("Bienvenidos al curso de EDA");
     }
 }
